231b6226082cacf67639feec1a505d555434579d
li-shuang1997 <897853031@qq.com>
2022-04-27 14:02:32 +0800

    first commit

---
+++ views/other/pageOne.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+       这是pageOne页面 
+    </div>
+</template>
+<script>
+    export default{
+        name:'PageOne',
+        data(){
+            return{}
+        }
+    }
+</script>
